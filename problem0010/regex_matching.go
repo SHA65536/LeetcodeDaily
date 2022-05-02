b7a253cab0acdb7e909ef48773d156ebac4272dc
SHA65336 <53488697+SHA65536@users.noreply.github.com>
2022-05-02 19:24:27 +0300

    added test cases for problem 10, this is gonna be hard

---
+++ problem0010/regex_matching.go
@@ -0,0 +1,13 @@
+package problem0010
+
+/*
+Given an input string s and a pattern p, implement regular expression matching with support for '.' and '*' where:
+'.' Matches any single character.​​​​
+'*' Matches zero or more of the preceding element.
+
+The matching should cover the entire input string (not partial).
+*/
+
+func isMatch(s string, p string) bool {
+	return false
+}
---
+++ problem0010/regex_matching_test.go
@@ -0,0 +1,46 @@
+package problem0010
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+type Result struct {
+	Str      string
+	Pat      string
+	Expected bool
+}
+
+/*
+Constraints:
+   1 <= s.length <= 20
+   1 <= p.length <= 30
+   s contains only lowercase English letters.
+   p contains only lowercase English letters, '.', and '*'.
+   It is guaranteed for each appearance of the character '*', there will be a previous valid character to match.
+*/
+
+var Results = []Result{
+	{"aa", "a", false},
+	{"aa", "a*", true},
+	{"ab", ".b", true},
+	{"ab", ".*", true},
+	{"aaaa", "aa*", true},
+	{"abc", "a", false},
+	{"abbd", "ab*d", true},
+	{"acd", "ab*d", true},
+	{"abbcccd", "acd", false},
+	{"a", "aaa*", false},
+}
+
+func TestIsMatch(t *testing.T) {
+	assert := assert.New(t)
+
+	for _, res := range Results {
+		want := res.Expected
+		got := isMatch(res.Str, res.Pat)
+		assert.Equal(want, got, fmt.Sprintf("%+v", res))
+	}
+}

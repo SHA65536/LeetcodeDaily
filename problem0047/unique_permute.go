71b21dac762c02d94acd94ce41245ac1812592d9
SHA65536 <53488697+SHA65536@users.noreply.github.com>
2022-05-12 23:39:49 +0300

    improved efficiency of 47

--- problem0047/unique_permute.go
+++ problem0047/unique_permute.go
@@ -1,7 +1,5 @@
 package problem0047
 
-import "fmt"
-
 /*
 https://leetcode.com/problems/permutations-ii
 
@@ -13,32 +11,24 @@ type void interface{}
 var member void
 
 func permuteUnique(nums []int) [][]int {
-	var res = [][]int{}
-	var seen = map[string]void{}
-	var sign string
-	for _, perm := range permute(nums) {
-		sign = fmt.Sprint(perm)
-		if _, ok := seen[sign]; !ok {
-			seen[sign] = member
-			res = append(res, perm)
-		}
-	}
-	return res
-}
-
-func permute(nums []int) [][]int {
 	var length int = len(nums)
 	var res = [][]int{}
 	var others []int
+	var seen = map[int]void{}
 	if length == 1 {
 		return [][]int{{nums[0]}}
 	}
 	for i, num := range nums {
+		if _, ok := seen[num]; !ok {
+			seen[num] = member
+		} else {
+			continue
+		}
 		others = make([]int, length)
 		copy(others, nums)
 		others[i] = others[length-1]
 		others = others[:length-1]
-		for _, perm := range permute(others) {
+		for _, perm := range permuteUnique(others) {
 			cur := append([]int{num}, perm...)
 			res = append(res, cur)
 		}

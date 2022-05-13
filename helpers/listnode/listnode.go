c063422092fb700589dcebe3adc816f21aa25be2
SHA65536 <53488697+SHA65536@users.noreply.github.com>
2022-05-13 19:18:24 +0300

    fixed list generation and printing

--- helpers/listnode/listnode.go
+++ helpers/listnode/listnode.go
@@ -9,6 +9,9 @@ type ListNode struct {
 
 // MakeListNode constructs a ListNode from given integers
 func MakeListNode(nums ...int) *ListNode {
+	if len(nums) == 0 {
+		return nil
+	}
 	resList := &ListNode{}
 	cur := resList
 	for idx, num := range nums {
@@ -23,10 +26,12 @@ func MakeListNode(nums ...int) *ListNode {
 }
 
 func (ln *ListNode) String() string {
+	if ln == nil {
+		return ""
+	}
 	if ln.Next != nil {
 		return fmt.Sprintf("%d, %s", ln.Val, ln.Next.String())
-
-	} else {
-		return fmt.Sprint(ln.Val)
 	}
+	return fmt.Sprint(ln.Val)
+
 }

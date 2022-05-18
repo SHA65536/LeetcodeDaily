78726d60788c5412052b8620d2eb405efbc416f5
SHA65536 <53488697+SHA65536@users.noreply.github.com>
2022-05-18 14:50:11 +0300

    removed print statements

--- problem0334/reverse_string.go
+++ problem0334/reverse_string.go
@@ -1,7 +1,5 @@
 package problem0334
 
-import "fmt"
-
 /*
 https://leetcode.com/problems/reverse-string/
 
@@ -12,10 +10,8 @@ You must do this by modifying the input array in-place with O(1) extra memory.
 
 func reverseString(s []byte) {
 	var start, end = 0, len(s) - 1
-	fmt.Println(string(s), "\n========")
 	for end > start {
 		s[start], s[end] = s[end], s[start]
-		fmt.Println(string(s))
 		start++
 		end--
 	}

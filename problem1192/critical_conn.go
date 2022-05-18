7d221a99b6c8f9111950e46dd9cb552c73e9c65a
SHA65536 <53488697+SHA65536@users.noreply.github.com>
2022-05-18 19:26:54 +0300

    finished problem 1192

--- problem1192/critical_conn.go
+++ problem1192/critical_conn.go
@@ -10,11 +10,32 @@ Return all critical connections in the network in any order.
 */
 
 func criticalConnections(n int, connections [][]int) [][]int {
-	var res = [][]int{}
 	var nodes = map[int][]int{}
+	var visited = map[int]int{}
 	for _, conn := range connections {
 		nodes[conn[0]] = append(nodes[conn[0]], conn[1])
 		nodes[conn[1]] = append(nodes[conn[1]], conn[0])
+	}
+	return search(0, -1, 0, nodes, visited)
+}
+
+func search(cur, prev, depth int, nodes map[int][]int, visited map[int]int) [][]int {
+	var res = [][]int{}
+	visited[cur] = depth
+	for _, rt := range nodes[cur] {
+		if rt == prev {
+			continue
+		}
+		if _, ok := visited[rt]; !ok {
+			nxt := search(rt, cur, depth+1, nodes, visited)
+			res = append(res, nxt...)
+		}
+		if visited[cur] > visited[rt] {
+			visited[cur] = visited[rt]
+		}
+		if visited[rt] > depth {
+			res = append(res, []int{cur, rt})
+		}
 	}
 	return res
 }

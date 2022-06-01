694157eaa9e6e7c51994134e20aa067f89731c12
SHA65536 <53488697+SHA65536@users.noreply.github.com>
2022-06-01 19:27:16 +0300

    renamed test function

--- problem1480/running_sum_test.go
+++ problem1480/running_sum_test.go
@@ -17,7 +17,7 @@ var Results = []Result{
 	{[]int{1, 1, 1, 1}, []int{1, 2, 3, 4}},
 }
 
-func TestMergeTwoLists(t *testing.T) {
+func TestRunningSum(t *testing.T) {
 	assert := assert.New(t)
 
 	for _, res := range Results {

e1bd649f43e289c6ce1e54d68adbe99964ec418c
SHA65536 <53488697+SHA65536@users.noreply.github.com>
2022-06-03 16:27:25 +0300

    removed cache from 304

--- problem0304/range_sum.go
+++ problem0304/range_sum.go
@@ -15,24 +15,19 @@ upper left corner (row1, col1) and lower right corner (row2, col2).
 
 type NumMatrix struct {
 	Matrix [][]int
-	Cache  map[[4]int]int
 }
 
 func Constructor(matrix [][]int) NumMatrix {
-	return NumMatrix{matrix, make(map[[4]int]int)}
+	return NumMatrix{matrix}
 }
 
 func (this *NumMatrix) SumRegion(row1 int, col1 int, row2 int, col2 int) int {
 	var sum int
-	if val, ok := this.Cache[[4]int{row1, col1, row2, col2}]; ok {
-		return val
-	}
 	for i := row1; i <= row2; i++ {
 		for j := col1; j <= col2; j++ {
 			sum += this.Matrix[i][j]
 		}
 	}
-	this.Cache[[4]int{row1, col1, row2, col2}] = sum
 	return sum
 }
 
@@ -47,7 +42,7 @@ func ConstructorPrefix(matrix [][]int) NumMatrix {
 			}
 		}
 	}
-	return NumMatrix{matrix, nil}
+	return NumMatrix{matrix}
 }
 
 func (this *NumMatrix) SumRegionPrefix(row1 int, col1 int, row2 int, col2 int) int {

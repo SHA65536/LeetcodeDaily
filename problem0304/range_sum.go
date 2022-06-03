285752be0d949d9f1d80c2b0ab2f0e5188f8b66c
SHA65536 <53488697+SHA65536@users.noreply.github.com>
2022-06-03 16:13:51 +0300

    Optimized problem 304, added benchmarks

--- problem0304/range_sum.go
+++ problem0304/range_sum.go
@@ -35,3 +35,31 @@ func (this *NumMatrix) SumRegion(row1 int, col1 int, row2 int, col2 int) int {
 	this.Cache[[4]int{row1, col1, row2, col2}] = sum
 	return sum
 }
+
+func ConstructorPrefix(matrix [][]int) NumMatrix {
+	for i := 0; i < len(matrix); i++ {
+		var rollingSum int
+		for j := 0; j < len(matrix[0]); j++ {
+			rollingSum += matrix[i][j]
+			matrix[i][j] = rollingSum
+			if i > 0 {
+				matrix[i][j] += matrix[i-1][j]
+			}
+		}
+	}
+	return NumMatrix{matrix, nil}
+}
+
+func (this *NumMatrix) SumRegionPrefix(row1 int, col1 int, row2 int, col2 int) int {
+	var sum int = this.Matrix[row2][col2]
+	if row1 > 0 {
+		sum -= this.Matrix[row1-1][col2]
+	}
+	if col1 > 0 {
+		sum -= this.Matrix[row2][col1-1]
+	}
+	if row1 > 0 && col1 > 0 {
+		sum += this.Matrix[row1-1][col1-1]
+	}
+	return sum
+}
--- problem0304/range_sum_test.go
+++ problem0304/range_sum_test.go
@@ -12,14 +12,6 @@ type Result struct {
 	Expected int
 }
 
-var Matrix = [][]int{
-	{3, 0, 1, 4, 2},
-	{5, 6, 3, 2, 1},
-	{1, 2, 0, 1, 5},
-	{4, 1, 0, 1, 7},
-	{1, 0, 3, 0, 5},
-}
-
 var Results = []Result{
 	{[4]int{2, 1, 4, 3}, 8},
 	{[4]int{1, 1, 2, 2}, 11},
@@ -29,11 +21,71 @@ var Results = []Result{
 
 func TestSumRegion(t *testing.T) {
 	assert := assert.New(t)
+	var matrix = [][]int{
+		{3, 0, 1, 4, 2},
+		{5, 6, 3, 2, 1},
+		{1, 2, 0, 1, 5},
+		{4, 1, 0, 1, 7},
+		{1, 0, 3, 0, 5},
+	}
 
-	mat := Constructor(Matrix)
+	mat := Constructor(matrix)
 	for _, res := range Results {
 		want := res.Expected
 		got := mat.SumRegion(res.Query[0], res.Query[1], res.Query[2], res.Query[3])
 		assert.Equal(want, got, fmt.Sprintf("%+v", res))
+	}
+}
+
+func BenchmarkSumRegion(b *testing.B) {
+	var matrix = [][]int{
+		{3, 0, 1, 4, 2},
+		{5, 6, 3, 2, 1},
+		{1, 2, 0, 1, 5},
+		{4, 1, 0, 1, 7},
+		{1, 0, 3, 0, 5},
+	}
+	mat := Constructor(matrix)
+
+	for i := 0; i < b.N; i++ {
+		for _, res := range Results {
+			mat.SumRegion(res.Query[0], res.Query[1], res.Query[2], res.Query[3])
+		}
+	}
+}
+
+func TestSumRegionPrefix(t *testing.T) {
+	assert := assert.New(t)
+	var matrix = [][]int{
+		{3, 0, 1, 4, 2},
+		{5, 6, 3, 2, 1},
+		{1, 2, 0, 1, 5},
+		{4, 1, 0, 1, 7},
+		{1, 0, 3, 0, 5},
+	}
+
+	mat := ConstructorPrefix(matrix)
+	for _, res := range Results {
+		want := res.Expected
+		got := mat.SumRegionPrefix(res.Query[0], res.Query[1], res.Query[2], res.Query[3])
+		assert.Equal(want, got, fmt.Sprintf("%+v", res))
+	}
+}
+
+func BenchmarkSumRegionPrefix(b *testing.B) {
+	var matrix = [][]int{
+		{3, 0, 1, 4, 2},
+		{5, 6, 3, 2, 1},
+		{1, 2, 0, 1, 5},
+		{4, 1, 0, 1, 7},
+		{1, 0, 3, 0, 5},
+	}
+
+	mat := ConstructorPrefix(matrix)
+
+	for i := 0; i < b.N; i++ {
+		for _, res := range Results {
+			mat.SumRegionPrefix(res.Query[0], res.Query[1], res.Query[2], res.Query[3])
+		}
 	}
 }

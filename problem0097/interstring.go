516ecba8740b7ce280b206c57ca1e1969229d99d
SHA65536 <53488697+SHA65536@users.noreply.github.com>
2022-07-07 22:53:22 +0300

    added prolblem 97

--- problem0097/interstring.go
+++ problem0097/interstring.go
@@ -10,8 +10,40 @@ t = t1 + t2 + ... + tm
 |n - m| <= 1
 The interleaving is s1 + t1 + s2 + t2 + s3 + t3 + ... or t1 + s1 + t2 + s2 + t3 + s3 + ...
 Note: a + b is the concatenation of strings a and b.
+
+Follow up: Could you solve it using only O(s2.length) additional memory space?
 */
 
 func isInterleave(s1 string, s2 string, s3 string) bool {
-	return true
+	var smaller, bigger *string = &s1, &s1
+	var lenSmaller, lenBigger int
+	if len(s1)+len(s2) != len(s3) {
+		return false
+	}
+	if len(s1) > len(s2) {
+		smaller = &s2
+	} else {
+		bigger = &s2
+	}
+	lenSmaller = len(*smaller)
+	lenBigger = len(*bigger)
+	var cache = make([]bool, lenSmaller+1)
+	cache[0] = true
+	for i := 1; i <= lenSmaller && cache[i-1]; i++ {
+		cache[i] = s2[i-1] == s3[i-1]
+	}
+	for i := 1; i <= lenBigger; i++ {
+		cache[0] = cache[0] && s1[i-1] == s3[i-1]
+		for j := 1; j <= lenSmaller; j++ {
+			var s1Match, s2Match bool
+			if s1[i-1] == s3[i+j-1] {
+				s1Match = cache[j]
+			}
+			if s2[j-1] == s3[i+j-1] {
+				s2Match = cache[j-1]
+			}
+			cache[j] = s1Match || s2Match
+		}
+	}
+	return cache[len(cache)-1]
 }

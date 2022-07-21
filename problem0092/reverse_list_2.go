021685e9dd3347f639f3fc869f2b10c2058683ef
SHA65536 <53488697+SHA65536@users.noreply.github.com>
2022-07-21 17:51:50 +0300

    added problem 92

--- problem0092/reverse_list_2.go
+++ problem0092/reverse_list_2.go
@@ -7,6 +7,20 @@ Given the head of a singly linked list and two integers left and right where lef
 reverse the nodes of the list from position left to position right, and return the reversed list.
 */
 
+// I went for the 2 pass approach, difference in performance is negligible and its much more readable
 func reverseBetween(head *ListNode, left int, right int) *ListNode {
-	return nil
+	var values []int
+	for cur := head; cur != nil; cur = cur.Next {
+		values = append(values, cur.Val)
+	}
+	cur := head
+	for idx := range values {
+		if idx >= left-1 && idx <= right-1 {
+			cur.Val = values[(right-1)-(idx-(left-1))]
+		} else {
+			cur.Val = values[idx]
+		}
+		cur = cur.Next
+	}
+	return head
 }

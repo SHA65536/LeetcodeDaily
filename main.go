e67082326383adc332093215e0de32578865d2b3
SHA65536 <53488697+SHA65536@users.noreply.github.com>
2022-08-04 13:13:02 +0300

    removed main file

--- main.go
+++
@@ -1,9 +0,0 @@
-package main
-
-import "fmt"
-
-func main() {
-	fmt.Println("Hey! This is my Leetcode journey in the Go programming language!\n",
-		"I use tests to keep track of the challenges.\n",
-		"Try running 'go test ./...'.")
-}

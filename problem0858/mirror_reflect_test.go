9c4bf30657352b5928aacec82a8f4d63262a03fa
SHA65536 <53488697+SHA65536@users.noreply.github.com>
2022-08-04 13:20:30 +0300

    added problem 858

---
+++ problem0858/mirror_reflect_test.go
@@ -0,0 +1,28 @@
+package problem0858
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+type Result struct {
+	P, Q     int
+	Expected int
+}
+
+var Results = []Result{
+	{2, 1, 2},
+	{3, 1, 1},
+}
+
+func TestKthSmallest(t *testing.T) {
+	assert := assert.New(t)
+
+	for _, res := range Results {
+		want := res.Expected
+		got := mirrorReflection(res.P, res.Q)
+		assert.Equal(want, got, fmt.Sprintf("%+v", res))
+	}
+}

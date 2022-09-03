a91188acdea4d9754f05bad46918ee24c4aaf55d
SHA65536 <53488697+SHA65536@users.noreply.github.com>
2022-09-03 11:27:17 +0300

    added comments to 967

--- problem0967/digit_diff.go
+++ problem0967/digit_diff.go
@@ -8,6 +8,7 @@ You may return the answer in any order.
 
 func numsSameConsecDiff(n int, k int) []int {
 	var results = make([]int, 0)
+	// Checking each starting digits excluding 0 as we're not allowed leading zeros
 	for i := 1; i < 10; i++ {
 		calcDigitDiff(&results, i, n-1, k)
 	}
@@ -16,13 +17,17 @@ func numsSameConsecDiff(n int, k int) []int {
 
 func calcDigitDiff(results *[]int, cur, left, diff int) {
 	if left == 0 {
+		// If we reached the desired length, add to results
 		*results = append(*results, cur)
 		return
 	}
 	if (cur%10)+diff <= 9 {
+		// If adding the difference results in a valid number, keep checking
 		calcDigitDiff(results, (cur*10)+(cur%10)+diff, left-1, diff)
 	}
+	// If the diffrence is 0, subtracting and adding will result in the same number
 	if diff != 0 && (cur%10)-diff >= 0 {
+		// If subtracting the difference results in a valid number, keep checking
 		calcDigitDiff(results, (cur*10)+(cur%10)-diff, left-1, diff)
 	}
 }

98976ae8ddd73975c91720463347c457e2b2f9fa
SHA65536 <53488697+SHA65536@users.noreply.github.com>
2022-11-03 18:32:58 +0200

    ddded problem 538

---
+++ problems/problem0538/bst_to_greater_test.go
@@ -0,0 +1,32 @@
+package problem0538
+
+import (
+	"fmt"
+	"testing"
+
+	. "leetcodedaily/helpers/binarytree"
+
+	"github.com/stretchr/testify/assert"
+)
+
+type Result struct {
+	Input    *TreeNode
+	Expected *TreeNode
+}
+
+var Results = []Result{
+	{MakeTree(4, 1, 0, NULL, NULL, 2, NULL, 3, NULL, NULL, 6, 5, NULL, NULL, 7, NULL, 8, NULL, NULL),
+		MakeTree(30, 36, 36, NULL, NULL, 35, NULL, 33, NULL, NULL, 21, 26, NULL, NULL, 15, NULL, 8, NULL, NULL)},
+	{MakeTree(0, NULL, 1, NULL, NULL),
+		MakeTree(1, NULL, 1, NULL, NULL)},
+}
+
+func TestBstToGreaterTree(t *testing.T) {
+	assert := assert.New(t)
+
+	for _, res := range Results {
+		want := res.Expected
+		got := convertBST(res.Input)
+		assert.Equal(want, got, fmt.Sprintf("%+v", res))
+	}
+}

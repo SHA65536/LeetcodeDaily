1001316a3662b71fadfeecb1b4baf4ff353fd28b
SHA65536 <53488697+SHA65536@users.noreply.github.com>
2022-11-03 18:36:28 +0200

    added problem 1038

---
+++ problems/problem1038/bst_to_greater.go
@@ -0,0 +1,27 @@
+package problem1038
+
+import . "leetcodedaily/helpers/binarytree"
+
+/*
+Given the root of a Binary Search Tree (BST), convert it to a Greater Tree
+such that every key of the original BST is changed to the original key plus the sum of all keys greater than the original key in BST.
+As a reminder, a binary search tree is a tree that satisfies these constraints:
+The left subtree of a node contains only nodes with keys less than the node's key.
+The right subtree of a node contains only nodes with keys greater than the node's key.
+Both the left and right subtrees must also be binary search trees.
+*/
+
+func bstToGst(root *TreeNode) *TreeNode {
+	var sum int
+	convertHelper(root, &sum)
+	return root
+}
+
+func convertHelper(root *TreeNode, sum *int) {
+	if root != nil {
+		convertHelper(root.Right, sum)
+		*sum += root.Val
+		root.Val = *sum
+		convertHelper(root.Left, sum)
+	}
+}

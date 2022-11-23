f94e7067e09d2959cb79eb90844afe4e73129d02
SHA65536 <53488697+SHA65536@users.noreply.github.com>
2022-11-24 01:28:04 +0200

    added comments to sudoku

--- problems/problem0036/valid_sudoku.go
+++ problems/problem0036/valid_sudoku.go
@@ -10,41 +10,39 @@ A Sudoku board (partially filled) could be valid but is not necessarily solvable
 Only the filled cells need to be validated according to the mentioned rules.
 */
 
-const One byte = '1'
-
 func isValidSudoku(board [][]byte) bool {
 	// Checking rows and cols
 	for r := 0; r < 9; r++ {
-		row := make([]int, 9)
-		col := make([]int, 9)
+		row := make([]bool, 9) // row[i] shows wether we have seen i in this row already
+		col := make([]bool, 9) // col[i] shows wether we have seen i in this col already
 		for c := 0; c < 9; c++ {
 			if board[r][c] != '.' {
-				if row[board[r][c]-One] == 1 {
-					return false
+				if row[board[r][c]-'1'] { // If already seen
+					return false // Invalid
 				}
-				row[board[r][c]-One] = 1
+				row[board[r][c]-'1'] = true // Set to seen
 			}
 			if board[c][r] != '.' {
-				if col[board[c][r]-One] == 1 {
-					return false
+				if col[board[c][r]-'1'] { // If already seen
+					return false // Invalid
 				}
-				col[board[c][r]-One] = 1
+				col[board[c][r]-'1'] = true // Set to seen
 			}
 		}
 	}
 	// Checking squares
-	for r := 0; r < 9; r += 3 {
-		for c := 0; c < 9; c += 3 {
-			row := make([]int, 9)
-			for i := 0; i < 3; i++ {
-				for j := 0; j < 3; j++ {
+	for r := 0; r < 9; r += 3 { // r is the starting row of the inner square
+		for c := 0; c < 9; c += 3 { // c is the starting col of the inner square
+			row := make([]bool, 9)
+			for i := 0; i < 3; i++ { // i is the row of the cell inside the square
+				for j := 0; j < 3; j++ { // j is the col of the cell inside the square
 					if board[r+i][c+j] == '.' {
 						continue
 					}
-					if row[board[r+i][c+j]-One] == 1 {
-						return false
+					if row[board[r+i][c+j]-'1'] { // If already seen
+						return false // Invalid
 					}
-					row[board[r+i][c+j]-One] = 1
+					row[board[r+i][c+j]-'1'] = true // Set to seen
 				}
 			}
 		}

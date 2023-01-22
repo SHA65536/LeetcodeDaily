8e761cb31d0eaaa05e5b301fd3c0ed94a3c8e72e
SHA65536 <53488697+SHA65536@users.noreply.github.com>
2023-01-22 18:15:56 +0200

    added caching to 131

--- problems/problem0131/pali_part.go
+++ problems/problem0131/pali_part.go
@@ -26,6 +26,38 @@ func partition(s string) [][]string {
 	return res
 }
 
+func partitionCache(s string) [][]string {
+	var dfs func(string) [][]string
+	var cache = map[string][][]string{}
+
+	dfs = func(s string) [][]string {
+		if v, ok := cache[s]; ok {
+			return v
+		}
+		var res [][]string
+		if len(s) == 0 {
+			return res
+		}
+		for i := 1; i <= len(s); i++ {
+			if isPalindrome(s[:i]) {
+				temp := dfs(s[i:])
+				for _, cur := range temp {
+					var n = []string{s[:i]}
+					n = append(n, cur...)
+					res = append(res, n)
+				}
+			}
+		}
+		if isPalindrome(s) {
+			res = append(res, []string{s})
+		}
+		cache[s] = res
+		return res
+	}
+
+	return dfs(s)
+}
+
 func isPalindrome(s string) bool {
 	for i := 0; i < len(s)/2; i++ {
 		if s[i] != s[len(s)-i-1] {
--- problems/problem0131/pali_part_test.go
+++ problems/problem0131/pali_part_test.go
@@ -16,6 +16,11 @@ var Results = []Result{
 	{"aab", [][]string{{"a", "a", "b"}, {"aa", "b"}}},
 	{"a", [][]string{{"a"}}},
 	{"aaa", [][]string{{"a", "a", "a"}, {"a", "aa"}, {"aa", "a"}, {"aaa"}}},
+	{"brjwevkhxozncuzy", [][]string{{"b", "r", "j", "w", "e", "v", "k", "h", "x", "o", "z", "n", "c", "u", "z", "y"}}},
+	{"kpethkuaolsjsody", [][]string{{"k", "p", "e", "t", "h", "k", "u", "a", "o", "l", "s", "j", "s", "o", "d", "y"}, {"k", "p", "e", "t", "h", "k", "u", "a", "o", "l", "sjs", "o", "d", "y"}}},
+	{"jjacqmughpbxpfhp", [][]string{{"j", "j", "a", "c", "q", "m", "u", "g", "h", "p", "b", "x", "p", "f", "h", "p"}, {"jj", "a", "c", "q", "m", "u", "g", "h", "p", "b", "x", "p", "f", "h", "p"}}},
+	{"sxuhxdlqybxamsmz", [][]string{{"s", "x", "u", "h", "x", "d", "l", "q", "y", "b", "x", "a", "m", "s", "m", "z"}, {"s", "x", "u", "h", "x", "d", "l", "q", "y", "b", "x", "a", "msm", "z"}}},
+	{"aaaaa", [][]string{{"a", "a", "a", "a", "a"}, {"a", "a", "a", "aa"}, {"a", "a", "aa", "a"}, {"a", "a", "aaa"}, {"a", "aa", "a", "a"}, {"a", "aa", "aa"}, {"a", "aaa", "a"}, {"a", "aaaa"}, {"aa", "a", "a", "a"}, {"aa", "a", "aa"}, {"aa", "aa", "a"}, {"aa", "aaa"}, {"aaa", "a", "a"}, {"aaa", "aa"}, {"aaaa", "a"}, {"aaaaa"}}},
 }
 
 func TestPartitionPalindrome(t *testing.T) {
@@ -25,5 +30,29 @@ func TestPartitionPalindrome(t *testing.T) {
 		want := res.Expected
 		got := partition(res.Input)
 		assert.Equal(want, got, fmt.Sprintf("%+v", res))
+	}
+}
+func BenchmarkPartitionPalindrome(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		for _, res := range Results {
+			partition(res.Input)
+		}
+	}
+}
+
+func TestPartitionPalindromeCache(t *testing.T) {
+	assert := assert.New(t)
+
+	for _, res := range Results {
+		want := res.Expected
+		got := partitionCache(res.Input)
+		assert.Equal(want, got, fmt.Sprintf("%+v", res))
+	}
+}
+func BenchmarkPartitionPalindromeCache(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		for _, res := range Results {
+			partitionCache(res.Input)
+		}
 	}
 }

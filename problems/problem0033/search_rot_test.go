a9d58d755e86fb7e26c6be45b8fdab9a34af69f1
SHA65536 <53488697+SHA65536@users.noreply.github.com>
2023-08-08 21:39:51 +0300

    better tests for 33

--- problems/problem0033/search_rot_test.go
+++ problems/problem0033/search_rot_test.go
@@ -2,6 +2,7 @@ package problem0033
 
 import (
 	"fmt"
+	"math/rand"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -18,9 +19,24 @@ var Results = []Result{
 	{[]int{4, 5, 6, 7, 0, 1, 2}, 3, -1},
 	{[]int{1}, 0, -1},
 	{[]int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27}, 19, 19},
-	{[]int{17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16}, 15, 26},
-	{[]int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70}, 40, 40},
-	{[]int{43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42}, 40, 68},
+	createArray(1000),
+	createArray(100000),
+	createArray(10000000),
+}
+
+func createArray(length int) Result {
+	var res = Result{
+		Input:  make([]int, length),
+		Target: rand.Intn(length),
+	}
+	var pivot = rand.Intn(length)
+	for i := range res.Input {
+		res.Input[(i+pivot)%length] = i
+		if i == res.Target {
+			res.Expected = (i + pivot) % length
+		}
+	}
+	return res
 }
 
 func TestSearchInRotated(t *testing.T) {
